7890a8f1f4b0d7b61c01729c60077eed806e7cdd
Muhammad Amjad <muhammadamjad7655@gmail.com>
2024-04-10 22:18:56 +0500

    sharing code of number guessing game

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+//take random number from computer
+async function RandomNum() {
+    
+const randomNum= Math.floor(Math.random()*10+1);
+
+//take input from user to guess
+
+const answer=await inquirer.prompt([
+    {
+        name: "userGuess",
+        type:"number",
+        message:"Please guess a number:",
+    }
+])
+//compare both numbers for equality
+if (answer.userGuess===randomNum) {
+    console.log("congratulations...! You guessed it right.");
+} else{
+    console.log("Sorry, you guessed it wrong");
+    
+}
+}
+
+RandomNum()
